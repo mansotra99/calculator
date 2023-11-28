951704fec8c4621f77f037534a4b6cdaa086e0a6
Kaneshwar sharma <66859748+mansotra99@users.noreply.github.com>
2023-11-29 01:49:01 +0530

    Create subtract.py

---
+++ subtract.py
@@ -0,0 +1,36 @@
+import json
+
+def lambda_handler(event, context):
+    try:
+        # Parse input from the API Gateway event
+        num1 = int(event.get('num1'))
+        num2 = int(event.get('num2'))
+        if num1 is None or num2 is None:
+            raise ValueError("Both 'num1' and 'num2' must be provided as numbers")
+
+        # Perform the subtraction operation
+        result = num1 - num2
+
+        # Return the result as a JSON response
+        response = {
+            "statusCode": 200,
+            "body": {"result": result}
+        }
+
+        return response
+    except ValueError as ve:
+        # Handle validation errors
+        response = {
+            "statusCode": 400,
+            "body": json.dumps({"error": str(ve)})
+        }
+
+        return response
+    except Exception as e:
+        # Handle errors
+        response = {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
+
+        return response
